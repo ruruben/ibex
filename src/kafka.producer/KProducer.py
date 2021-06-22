4bfc7e580057afa56d0c81aaba31ce61d7e965d2
Ruben <gutyruben@gmail.com>
2021-06-22 15:23:26 +0200

    kafka producer

---
+++ src/kafka.producer/KProducer.py
@@ -0,0 +1,54 @@
+import os
+import time, random
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+def parseFileLine(line):
+
+    line_splitted = line.split(",")
+
+    ticker = line_splitted[0]
+    date = line_splitted[1]
+    close = line_splitted[2]
+    reference = line_splitted[3]
+    volume = line_splitted[4]
+    turnover = line_splitted[5]
+    last = line_splitted[6]
+    high = float(line_splitted[7])
+    low = float(line_splitted[8])
+    average = line_splitted[9]
+
+    value1 = close
+    value2 = round(random.uniform(low, high), 4)
+    print(ticker + ',' + date + ',' + line_splitted[2])
+    return str(ticker + ',' + date + ',' + value1).encode('utf-8')
+
+path = '/home/ruben/Escritorio/tfm/ibex/src/kafka.producer/IBEX35_Individual_data/'
+fileANA = os.path.join(path, 'ACCIONA.csv')
+fileTEF = os.path.join(path, 'TELEFONICA.csv')
+fileACX = os.path.join(path, 'ACERINOX.csv')
+infileAna = open(fileANA, 'r')
+infileTEL = open(fileTEF, 'r')
+infileACX = open(fileACX, 'r')
+
+for i in range(240):
+    print(i)
+    time.sleep(5)
+    try:
+        producer.send("IBEX35", parseFileLine(infileAna.readlines(1)[0]))
+    except:
+        print("First line")
+    try:
+        producer.send("IBEX35", parseFileLine(infileTEL.readlines(1)[0]))
+    except:
+        print("First line")
+    try:
+        producer.send("IBEX35", parseFileLine(infileACX.readlines(1)[0]))
+    except:
+        print("First line")
+
+infileAna.close()
+infileTEL.close()
+infileACX.close()
+
